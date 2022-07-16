6ae10592f05b8746682a2638dece4924ac3b81bf
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-07-16 15:54:39 -0500

    style: fixed lint issue

--- data/utilities/getRequestBody.ts
+++ data/utilities/getRequestBody.ts
@@ -1,5 +1,6 @@
 export async function getRequestBody(
   request: Request,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
 ): Promise<Record<string, any>> {
   try {
     return await request.clone().json();
