42a5d0bfa22dc74e947aea1a58204835e385d06c
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-07-16 16:48:38 -0500

    feat: multiple entries to reduce bundle size

--- .viterc.ts
+++ .viterc.ts
@@ -1,9 +1,16 @@
 import {defineConfig} from '@micra/vite-config/library';
+import {resolve} from 'path';
+
+const cwd = (...paths: string[]) => resolve(process.cwd(), ...paths);
 
 export default defineConfig({
   build: {
     rollupOptions: {
       external: ['@micra/core', '@micra/error'],
+      input: {
+        index: cwd('index.ts'),
+        utilities: cwd('utilities.ts'),
+      },
     },
   },
 });
